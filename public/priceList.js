dd20c4dae41ae1cafac93725e0e013273757b2c6
unknown <iosoloiowow@gmail.com>
2020-01-14 17:56:33 +0100

    updated prices

--- public/priceList.js
+++ public/priceList.js
@@ -1,27 +1,27 @@
 module.exports = {
 getPrice: function getPrice(cart) {
-    if (cart.itemName == "Item 1"){
+    if (cart.itemName == "Epli Poster N.1"){
       cart.price=10;
     } 
-    else if (cart.itemName == "Item 2") {
+    else if (cart.itemName == "Epli Poster N.2") {
       cart.price=20;
     }
-    else if (cart.itemName == "Item 3") {
+    else if (cart.itemName == "Epli Mug N.1") {
       cart.price=30;
     }
-    else if (cart.itemName == "Item 4") {
+    else if (cart.itemName == "Epli Mug N.2") {
       cart.price=40;
     }
-    else if (cart.itemName == "Item 5") {
+    else if (cart.itemName == "Epli Daki N.1") {
       cart.price=50;
     }
-    else if (cart.itemName == "Item 6") {
+    else if (cart.itemName == "Epli Daki N.2") {
       cart.price=60;
     }
-    else if (cart.itemName == "Item 7") {
+    else if (cart.itemName == "Super Rare Epli Fanart") {
       cart.price=70;
     }
-    else if (cart.itemName == "Item 8") {
+    else if (cart.itemName == "Redacted") {
       cart.price=80;
     }
   }
